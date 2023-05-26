cbd2c75ce1ce310ee3ed393bf6095f10aefaaeda
Prins Harkhani <harkhaniprins2001@gmail.com>
2023-05-26 15:27:08 +0530

    implement reusable query hooks

--- src/pages/CharacterList.js
+++ src/pages/CharacterList.js
@@ -1,23 +1,8 @@
 import React from "react";
-import { useQuery, gql } from "@apollo/client";
-
-const GET_CHARACTERS = gql`
-  query {
-    characters {
-      results {
-        id
-        name
-        image
-      }
-    }
-  }
-`;
+import Characterhooks from "../hooks/Characterhooks";
 
 const CharacterList = () => {
-  const { error, loading, data } = useQuery(GET_CHARACTERS);
-
-  console.log({error, loading, data});
-
+  const { error, loading, data } = Characterhooks();
   if (error) return <div>Something Went Wrong!</div>;
   if (loading) return <div>Loading...</div>;
   return (
@@ -25,7 +10,7 @@ const CharacterList = () => {
       {data.characters.results.map((character) => {
         return (
           <div>
-            <img src={character.image} alt="character_image"/>
+            <img src={character.image} alt="character_image" />
             <h2>{character.name}</h2>
           </div>
         );
